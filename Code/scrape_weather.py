630329d03ff920d571aa3066eaaf00d905c861c9
Kelv1nYu <ykq1312@126.com>
2019-12-05 12:28:27 -0500

    scrape, preprocess and write it to file

--- Code/scrape_weather.py
+++ Code/scrape_weather.py
@@ -83,3 +83,56 @@ Weather = pd.concat([p1Data, JulyData, AugData, SepData, OctData, p4Data], axis 
 
 
 # %%
+bicycle17 = pd.read_csv("./DataSet/bicycle17.csv")
+bicycle17.columns = ['drop', 'Date', 'hour', 'cnt']
+bicycle17 = bicycle17.drop(columns = ['drop'])
+
+
+# %%
+def cleanDfTime(row):
+    hour = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+    thisamt = row["time"]
+    if (thisamt.strip() == "12:52 AM"): return hour[0]
+    elif (thisamt.strip() == "1:52 AM"): return hour[1]
+    elif (thisamt.strip() == "2:52 AM"): return hour[2]
+    elif (thisamt.strip() == "3:52 AM"): return hour[3]
+    elif (thisamt.strip() == "4:52 AM"): return hour[4]
+    elif (thisamt.strip() == "5:52 AM"): return hour[5]
+    elif (thisamt.strip() == "6:52 AM"): return hour[6]
+    elif (thisamt.strip() == "7:52 AM"): return hour[7]
+    elif (thisamt.strip() == "8:52 AM"): return hour[8]
+    elif (thisamt.strip() == "9:52 AM"): return hour[9]
+    elif (thisamt.strip() == "10:52 AM"): return hour[10]
+    elif (thisamt.strip() == "11:52 AM"): return hour[11]
+    elif (thisamt.strip() == "12:52 PM"): return hour[12]
+    elif (thisamt.strip() == "1:52 PM"): return hour[13]
+    elif (thisamt.strip() == "2:52 PM"): return hour[14]
+    elif (thisamt.strip() == "3:52 PM"): return hour[15]
+    elif (thisamt.strip() == "4:52 PM"): return hour[16]
+    elif (thisamt.strip() == "5:52 PM"): return hour[17]
+    elif (thisamt.strip() == "6:52 PM"): return hour[18]
+    elif (thisamt.strip() == "7:52 PM"): return hour[19]
+    elif (thisamt.strip() == "8:52 PM"): return hour[20]
+    elif (thisamt.strip() == "9:52 PM"): return hour[21]
+    elif (thisamt.strip() == "10:52 PM"): return hour[22]
+    elif (thisamt.strip() == "11:52 PM"): return hour[23]
+    else: return np.nan
+
+    return np.nan
+
+#%%
+Weather.columns = ['drop', 'time', 'TF', 'Humidity', 'Windspeed', 'weather', 'Date']
+Weather['hour'] = Weather.apply(cleanDfTime, axis = 1)
+Weather = Weather.drop(columns = ['drop', 'time'])
+Weather = Weather.dropna()
+Weather['hour'] = Weather['hour'].astype('int')
+# Weather.reset_index()
+
+# %%
+Final_data = Weather.merge(bicycle17, on = ['Date','hour'], how = 'left')
+Final_data['cnt'] = Final_data['cnt'].fillna(0)
+Final_data['cnt'] = Final_data['cnt'].astype('int')
+# Final_data.to_csv("hour2017.csv")
+
+# %%
+
