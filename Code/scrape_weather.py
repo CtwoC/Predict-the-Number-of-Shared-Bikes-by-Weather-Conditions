bd808188c7fe4cfafa7d9406d39686aed13255f6
Zichu <527200054@qq.com>
2019-12-04 19:57:56 -0500

    Update scrape_weather.py

--- Code/scrape_weather.py
+++ Code/scrape_weather.py
@@ -10,7 +10,7 @@ from selenium.common.exceptions import TimeoutException
 import re
 
 #%%
-driver = webdriver.Chrome('/Users/yukaiqi/Downloads/chromedriver')
+driver = webdriver.Chrome('/Users/chenzichu/Desktop/Adwin Lo/GWU_classes/DATS_6103_DataMining/Class08_WebScraping/chromedriver')
 try:
     driver.get("https://www.wunderground.com/history/daily/us/va/arlington-county/KDCA/date/2017-1-1")
     sleep(20)
@@ -21,40 +21,30 @@ except TimeoutException as ex:
 
 
 #%%
-tables = soup.findAll('table')
-tab = tables[2]
-for tr in tab.tbody.findAll('tr'):
-    for td in tr.findAll('td'):
-        text = td.getText() + '\n'
-        print(text)
+
+
+
 
 #%%
 testWeatherData = pd.DataFrame( columns=['date','time','temp','humidity','windspeed'])
 
 
-def gettime(soup,k):
-    time=soup.select('#inner-content > div.region-content-main > div.row > div:nth-child(5) > div:nth-child(1) > div > lib-city-history-observation > div > div.observation-table.ng-star-inserted > table > tbody > tr:nth-child('+str(k)+') > td.mat-cell.cdk-column-dateString.mat-column-dateString.mat-table-sticky.ng-star-inserted > span')
-    return(re.split(r'<|>',str(time[0]))[2] if (len(time)==1) else "error")
-    
-def gettemp(soup,k):
-    temp=soup.select('#inner-content > div.region-content-main > div.row > div:nth-child(5) > div:nth-child(1) > div > lib-city-history-observation > div > div.observation-table.ng-star-inserted > table > tbody > tr:nth-child('+str(k)+') > td.mat-cell.cdk-column-temperature.mat-column-temperature.ng-star-inserted > lib-display-unit > span > span.wu-value.wu-value-to')
-    return(re.split(r'<|>',str(temp[0]))[2] if (len(temp)==1) else "error")
-    
-def gethum(soup,k):
-    hum=soup.select('#inner-content > div.region-content-main > div.row > div:nth-child(5) > div:nth-child(1) > div > lib-city-history-observation > div > div.observation-table.ng-star-inserted > table > tbody > tr:nth-child('+str(k)+') > td.mat-cell.cdk-column-humidity.mat-column-humidity.ng-star-inserted > lib-display-unit > span > span.wu-value.wu-value-to')
-    return(re.split(r'<|>',str(hum[0]))[2] if (len(hum)==1) else "error")
+def get_table(soup):
+    tables = soup.findAll('table')
+    tab = tables[2]
+    weathertable=pd.DataFrame(columns=[])
+    daylist=[]
+    for tr in tab.tbody.findAll('tr'):
+        hourlist=[]
+        for td in tr.findAll('td'):
+            line=td.getText()
+            line = re.sub('[\xa0]', '', line)
+            hourlist.append(line)
+        daylist.append(hourlist)
+    return daylist
 
-def getwind(soup,k):
-    wind=soup.select('#inner-content > div.region-content-main > div.row > div:nth-child(5) > div:nth-child(1) > div > lib-city-history-observation > div > div.observation-table.ng-star-inserted > table > tbody > tr:nth-child('+str(k)+') > td.mat-cell.cdk-column-windSpeed.mat-column-windSpeed.ng-star-inserted > lib-display-unit > span > span.wu-value.wu-value-to')
-    return(re.split(r'<|>',str(wind[0]))[2] if (len(wind)==1) else "error")
 
-#%%
-for k in range(1,24):
-    time=gettime(soup,k)
-    temp=gettemp(soup,k)
-    hum=gethum(soup,k)
-    wind=getwind(soup,k)
-    testWeatherData = testWeatherData.append({'date':'2017-01-01','time':time,'temp':temp,'humidity':hum,'windspeed':wind}, ignore_index=True)
+dayweather = pd.DataFrame.from_records(daylist)
 
 
 
@@ -65,28 +55,25 @@ bicycle17=pd.read_csv('/Users/chenzichu/Desktop/ItDMProj/DataSet/bicycle17.csv')
 datelist=list(bicycle17['Date'])
 datelist=np.array(datelist)
 datelist=np.unique(datelist)
-datelist1=list(datelist[0:3])
-WeatherData = pd.DataFrame( columns=['date','time','temp','humidity','windspeed'])
+datelist2=list(datelist[56:90])
+#WeatherData = pd.DataFrame( columns=['date','time','temp','humidity','windspeed'])
 
 #%%
-frames=[testWeatherData]
-for date in datelist1:
+#frames=[]
+for date in datelist2:
     driver = webdriver.Chrome('/Users/chenzichu/Desktop/Adwin Lo/GWU_classes/DATS_6103_DataMining/Class08_WebScraping/chromedriver')
     url = "https://www.wunderground.com/history/daily/us/va/arlington-county/KDCA/date/" + date
-    try:
-        driver.get(url)
-        sleep(10)
-        soup = BeautifulSoup(driver.page_source, 'html5lib')
-    except TimeoutException as ex:
-        print(ex.Message)
-        webDriver.navigate().refresh()
-    for k in range(1,24):
-        time=gettime(soup,k)
-        temp=gettemp(soup,k)
-        hum=gethum(soup,k)
-        wind=getwind(soup,k)
-        WeatherData = WeatherData.append({'date':date,'time':time,'temp':temp,'humidity':hum,'windspeed':wind}, ignore_index=True)
+    driver.get(url)
+    sleep(10)
+    soup = BeautifulSoup(driver.page_source, 'html5lib')
     driver.quit()
+    daylist=get_table(soup)
+    dayweather = pd.DataFrame.from_records(daylist)
+    choose=dayweather.drop(columns=[2,4,6,7,8])
+    choose.columns=['time','temp','hum','wind','weather']
+    choose['date']=date
+    frames.append(choose)
+weatherframe=pd.concat(frames)
 
 
 
